d0d67f0b1f946c58140c5a8ddaac1548f72c4689
Hercules Dias Campos <hercules.diascampos@kidshacklabs.com>
2021-04-13 12:01:49 -0400

    Added movement functionality

--- src/player.py
+++ src/player.py
@@ -11,8 +11,24 @@ class Player():
 
         self.surf.fill(self.colour)
 
-    def update(self):
-        pass
+        self.vel = [0.25, 0.25] #rework!
+
+        self.max_time = 100
+        self.cur_time = 100
+        self.directions = ((0,-1),(1,0),(0,1),(-1,0))
+        self.cur_direction = None
+        self.command_queue = []
+
+    def update(self, delta):
+        keys = pk.get_pressed()
+
+        up = keys[K_w] or keys[K_UP]
+        right = keys[K_d] or keys[K_RIGHT]
+        down = keys[K_s] or keys[K_DOWN]
+        left = keys[K_a] or keys[K_LEFT]
+
+        self.rect.left += self.vel[0] * delta * (right - left) #REWORK
+        self.rect.top += self.vel[1] * delta * (down - up) #REWORK
 
     def render(self, target):
         target.blit(self.surf, self.rect)
--- src/scene.py
+++ src/scene.py
@@ -1,12 +1,25 @@
+from pygame.locals import *
+from pygame import Color, Rect, Surface
 from src.player import Player
 
 class Scene():
     def __init__(self, text_info = None, scene_info = None):
+        self.surf = Surface((800,800), SRCALPHA)
+        #GRID:
+        number_of_cells = 16
+        from pygame.draw import rect as pdr
+        for i in range(number_of_cells):
+            for j in range(number_of_cells):
+                if (i%2==0) ^ (j%2==0):
+                    pdr(self.surf, (255, 255, 255), Rect(i*50, j*50, 50, 50))
+        
+        #Player
         self.player = Player()
 
     def update(self, delta):
-        self.player.update()
+        self.player.update(delta)
     
     def render(self, target):
         target.fill((127, 127, 127))
+        target.blit(self.surf, (0,0))
         self.player.render(target)
